eed2313a338be23a73fd5c12070a4504fe73424c
Yoav Tzelnick <yoav.tzelnick@gmail.com>
2018-06-02 19:25:06 +0300

    Added undo option, added bootstrap for options page

--- options.js
+++ options.js
@@ -1,16 +1,29 @@
-let page = document.getElementById('buttonDiv');
+let page = document.getElementsByClassName('list-group')[0];
 function constructOptions() {
+  page.innerHTML = "";
   chrome.storage.sync.get(["blacklisted"], function(result) {
     let blacklisted = result.blacklisted;
     for (let item of blacklisted) {
+      let line = document.createElement('li');
+      line.classList.add("list-group-item");
+      line.innerHTML = item;
       let button = document.createElement('button');
-      button.innerHTML = item;
+      button.classList.add('btn');
+      button.classList.add('btn-primary');
+      button.style = "margin: 8px;";
+      button.innerHTML = "Undo";
       button.addEventListener('click', function() {
-        alert("Removing " + item);
-        // chrome.storage.sync.set({color: item}, function() {
-        // })
+        chrome.storage.sync.get("blacklisted", function(b) {
+          b = b.blacklisted;
+          var index = b.indexOf(item);
+          if (index !== -1) b.splice(index, 1);
+          chrome.storage.sync.set({"blacklisted": b}, function() {
+            constructOptions();
+          })
+        });
       });
-      page.appendChild(button);
+      line.appendChild(button);
+      page.appendChild(line);
     }
   });
 }
